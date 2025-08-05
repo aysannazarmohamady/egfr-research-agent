1c5e3d57fac73607bb89a31a8dd3789b5d9d5184
Aysan Nazarmohammadi <30371881+aysannazarmohamady@users.noreply.github.com>
2025-08-05 16:00:10 +0330

    Create implementations.py

---
+++ src/core/implementations.py
@@ -0,0 +1,6 @@
+from typing import List
+from .interfaces import (
+    IKeywordExtractor, ISourceRecommender, ISearchEngine, 
+    IContentAnalyzer, IReportGenerator,
+    SearchResult, SearchResultType, ResearchQuery, ResearchReport
+)
